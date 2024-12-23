196cdbe44f8c3181a4e78f0fe505b11e0c2d6451
Kent Rasmussen <hyprkookeez@gmail.com>
2024-12-23 11:29:08 +0900

    feat: add linting and formatting commands

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,18 @@
 // @ts-check
 
 import eslint from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
+  // Central Server
   {
+    files: ["packages/central-server/**/*.ts"],
     languageOptions: {
       parserOptions: {
-        project: ["./tsconfig.json", "./tsconfig.jest.json"],
+        project: ["packages/central-server/tsconfig.json"],
         tsconfigRootDir: import.meta.dirname,
       },
     },
@@ -18,8 +21,45 @@ export default tseslint.config(
       "@typescript-eslint/switch-exhaustiveness-check": "error",
     },
   },
+  // Game Server
   {
-    files: ["**/*.js"],
+    files: ["packages/game-server/**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: ["packages/game-server/tsconfig.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/strict-boolean-expressions": "error",
+      "@typescript-eslint/switch-exhaustiveness-check": "error",
+    },
+  },
+  // Types
+  {
+    files: ["packages/types/**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: ["packages/types/tsconfig.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/strict-boolean-expressions": "error",
+      "@typescript-eslint/switch-exhaustiveness-check": "error",
+    },
+  },
+  // Check JS files in services
+  {
+    files: ["packages/**/*.js", "services/**/*.js", "*.js"],
     extends: [tseslint.configs.disableTypeChecked],
-  }
+  },
+  // We're running everything using NodeJS
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
 );
---
+++ packages/central-server/eslint.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/strict-boolean-expressions": "error",
+      "@typescript-eslint/switch-exhaustiveness-check": "error",
+    },
+  },
+);
